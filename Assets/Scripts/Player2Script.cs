aea1622fc01982ebd5e807d14b7a812af08fea6c
maximusdrex <max@theschaefers.com>
2015-02-04 19:05:41 -0800

    First commit

---
+++ Assets/Scripts/Player2Script.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Script : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int numTouches = Input.touchCount;
+        int Speed = 2;
+        int movex, movey;
+        if (numTouches < 2)
+        {
+            for (int touchNum = 0; touchNum < numTouches; touchNum++)
+            {
+                Touch touch = Input.GetTouch(touchNum);
+                if (touch.position.x > ((Screen.width / 4) * 3))
+                {
+                    movex = -1;
+                    movey = 0;
+                }
+                else if (touch.position.x < ((Screen.width / 4) * 3) && touch.position.x >= Screen.width / 2)
+                {
+                    movex = 1;
+                    movey = 0;
+                }
+                else
+                {
+                    movex = 0;
+                    movey = 0;
+                }
+                rigidbody2D.velocity = new Vector2(movex * Speed, movey * Speed);
+            }
+        }
+    }
+}
